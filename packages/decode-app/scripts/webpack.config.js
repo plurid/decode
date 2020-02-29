ed8e7ad6f9b0aa439f5b255f30e1272ab1f07591
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-29 19:38:55 +0200

    decode-app > setup: package

--- packages/decode-app/scripts/webpack.config.js
+++ packages/decode-app/scripts/webpack.config.js
@@ -3,6 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
+const APP_DIR = path.resolve(__dirname, '../source/renderer');
+const MONACO_DIR = path.resolve(__dirname, '../node_modules/monaco-editor');
+
 
 
 module.exports = [
@@ -17,12 +20,12 @@ module.exports = [
             rules: [
                 {
                     test: /\.json$/,
-                    use: 'json-loader'
+                    use: 'json-loader',
                 },
                 {
                     test: /\.ts$/,
                     include: /source/,
-                    use: [{ loader: 'ts-loader' }]
+                    use: [{ loader: 'ts-loader' }],
                 },
             ],
         },
@@ -42,13 +45,37 @@ module.exports = [
         module: {
             rules: [
                 {
+                    test: /\.(jpe?g|gif|png|svg|woff|ttf|wav|mp3|.pdf)$/i,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: '[path][name].[hash].[ext]',
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.css$/,
+                    include: APP_DIR,
+                    use: [
+                        {
+                            loader: 'style-loader',
+                        },
+                        {
+                            loader: 'css-loader',
+                        },
+                    ],
+                },
+                {
+                    test: /\.css$/,
+                    include: MONACO_DIR,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
                     test: /\.ts(x?)$/,
                     include: /source/,
                     use: [{ loader: 'ts-loader' }]
-                },
-                {
-                    test: /\.css$/i,
-                    use: ['style-loader', 'css-loader'],
                 },
             ],
         },
@@ -68,6 +95,6 @@ module.exports = [
                 { from: './source/window/package.json', to: './package.json' },
                 { from: './source/assets/', to: './assets/' },
             ]),
-        ]
-    }
+        ],
+    },
 ];
