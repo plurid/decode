e9a705ee0179f489e67d20dedb1118ba9374c227
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-29 19:18:39 +0200

    decode-app > dev: renderer > Editor

--- packages/decode-app/source/renderer/modules/components/Editor/index.tsx
+++ packages/decode-app/source/renderer/modules/components/Editor/index.tsx
@@ -11,6 +11,8 @@ import {
     Theme,
 } from '@plurid/plurid-themes';
 
+import MonacoEditor from 'react-monaco-editor';
+
 import {
     StyledEditor,
 } from './styled';
@@ -57,9 +59,31 @@ const Editor: React.FC<EditorProperties> = (
         stateEditors,
     } = properties;
 
+    const monacoOptions = {
+        selectOnLineNumbers: true
+    };
+
 
     /** state */
     const [editor, setEditor] = useState<IEditor>();
+    const [code, setCode] = useState('');
+
+
+    /** handlers */
+    const onChange = (
+        newValue: any,
+        e: any,
+    ) => {
+        console.log('onChange', newValue, e);
+    }
+
+    const editorDidMount = (
+        editor: any,
+        monaco: any,
+    ) => {
+        console.log('editorDidMount', editor);
+        editor.focus();
+    }
 
 
     /** effects */
@@ -78,7 +102,16 @@ const Editor: React.FC<EditorProperties> = (
     /** render */
     return (
         <StyledEditor>
-
+            <MonacoEditor
+                width="800"
+                height="600"
+                language="javascript"
+                theme="vs-dark"
+                value={code}
+                options={monacoOptions}
+                onChange={onChange}
+                editorDidMount={editorDidMount}
+            />
         </StyledEditor>
     );
 }
